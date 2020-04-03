1bf1b72ecabd73e65f246fe0f116253961d0c45c
Tosun Sait <tosunsa@cronos.be>
2020-04-03 12:02:53 +0200

    person styled is added

---
+++ src/Person_styled/PersonStyled.js
@@ -0,0 +1,22 @@
+// jshint esversion: 9 
+import React from 'react';
+import Radium from 'radium';
+
+import './PersonStyled.css';
+
+const person = ( props ) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    };
+    return (
+        <div className="Person" style={style}>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
+};
+
+export default Radium(person);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+// jshint esversion: 6
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './AppStyled';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
